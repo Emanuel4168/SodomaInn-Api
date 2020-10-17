bdc1667106b1d1c3c62ca58dd8f71fec6b2642f6
Emanuel4168 <emanuel.l4168@gmail.com>
2020-10-17 15:16:58 -0600

    Agregadas clases de utileria HashUtility y ObjectMapper

---
+++ SodomaInn.Core/Utils/HashUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SodomaInn.Core.Utils
+{
+    public class HashUtility
+    {
+        public string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                var builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ SodomaInn.Core/Utils/ObjectMapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SodomaInn.Core.Utils
+{
+    public class ObjectMapper
+    {
+        public static D Map<S, D>(S source) where D : new()
+        {
+            D dest = new D();
+            PropertyInfo[] sourceProps = typeof(S).GetProperties();
+            PropertyInfo[] destProps = typeof(D).GetProperties();
+            foreach (PropertyInfo prop in sourceProps)
+            {
+                if (prop == null)
+                {
+                    continue;
+                }
+                PropertyInfo destProp = destProps.FirstOrDefault(p => p.Name == prop.Name);
+                if (destProp == null)
+                {
+                    continue;
+                }
+                destProp.SetValue(dest, prop.GetValue(source));
+
+            }
+            return dest;
+        }
+    }
+}
